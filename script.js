7be7cc811be6e9a3b9ca8c7fee1f288d6f1ff4cc
lucioccoiman <164024629+lucioccoiman@users.noreply.github.com>
2025-03-26 15:52:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAkghNY1-Tz6-K_OViu-U9aamy5-lOyND8",
+  authDomain: "preguntaya-94f9f.firebaseapp.com",
+  projectId: "preguntaya-94f9f",
+  storageBucket: "preguntaya-94f9f.firebasestorage.app",
+  messagingSenderId: "881212498544",
+  appId: "1:881212498544:web:d898c20c38422e74848ab5",
+  measurementId: "G-R037Y74M0M"
+databaseURL: "https://preguntaya-94f9f-default-rtdb.firebaseio.com/"
+};
